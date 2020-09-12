c8fd6035444e701681531f7ef2adb4d0bc02f358
Eliezer Weiss <elizww@gmail.com>
2020-09-12 21:40:34 +0300

    setup jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,6 +8,13 @@ import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      }      
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
